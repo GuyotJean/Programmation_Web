4bf1d7034e97a8fd51f89c6529153ab1051e0af3
elevesA24 <elevesA24@PC203-BL09.College_Andre-Grasset.local>
2025-05-22 19:41:13 -0400

    Test avant tp

--- garage-back/server.js
+++ garage-back/server.js
@@ -79,6 +79,41 @@ app.delete('/api/voitures/:id', async (req, res) => {
     }
 });
 
+//Route PUT pour Update
+app.put('/api/voitures/:id', async (req, res) => {
+    const { id } = req.params; //Recuperer l'id de la voiture
+    const { modele, prix, description, image, date, proprietaire} = req.body;
+
+    console.log("Données re¸ues : ", req.body);
+
+    if(!modele || !prix || !description || ! proprietaire || !image || !date) {
+        console.log("Champs manquants", {modele, prix, description, image, date, proprietaire})
+        return res.status(400).json({ Message: "Tous les champs sont requis"});
+    }
+
+    try {
+        //Log
+        console.log("MàJ de la BDD avec les valeurs : ", {modele, prix, description, proprietaire, image, date})
+
+        const result = await pool.query(
+            'UPDATE voiture SET modele = $1, prix = $2, description = $3, images = $4, date = $5, proprietaire = $6 WHERE id = $7 RETURNING *',
+            [modele, prix, description, image, date, proprietaire, id]
+        );
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ message: "Voiture non trouvée"});
+        }
+
+        console.log("Voiture MàJ : ", result.rows[0]);
+
+        res.status(200).json(result.rows[0]);
+    } catch(err) {
+        console.error("Erreur lors de la mise a jour de la voiture : ", err);
+        res.status(500).json({ message : 'Erreur serveur'});
+    }
+})
+
+//Écoute du port
 app.listen(port, () => {
     console.log(`Serveur démarré sur http://localhost:${port}`);
   });
--- garage-front/src/ModifVoiture.js
+++ garage-front/src/ModifVoiture.js
@@ -1,25 +1,67 @@
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 
 function ModifVoiture() {
-
 const location = useLocation();
 const { id } = useParams();
+const navigate = useNavigate();
+
 const voiture = location.state;
 
-console.log('voiture', voiture);
+//Initialiser data avec les valeurs de base
+const [formData, setFormData] = useState({
+    modele: voiture.modele,
+    prix: voiture.prix,
+    description: voiture.description,
+    image: voiture.images,
+    date: voiture.date,
+    proprietaire: voiture.proprietaire
+});
+
+const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+};
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await fetch(`http://localhost:3000/api/voitures/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+            const updated = await response.json();
+            console.log("Voiture mise à jour: ", updated);
+            navigate('/');
+        } else {
+            const err = await response.json();
+            alert(`Erreur : ${err.message}`);
+        }
+    } catch(error) {
+        console.error('Erreur lors de la mise a jour :', error);
+        alert('Une erreur est survenue');
+    }
+};
+
 
 return (
 <div>
     <h1>Modifier la {voiture.modele} de {voiture.proprietaire}</h1>
-    <form>
-        <input type = "text" name = "modele" placeholder={voiture.modele} required/>
-        <input type = "text" name = "prix" placeholder={voiture.prix} required/>
-        <input type = "text" name = "description" placeholder = {voiture.description} required/>
-        <input type = "text" name = "image" placeholder={voiture.image} required/>
-        <input type = "text" name = "date" placeholder={voiture.date} required/>
-        <input type = "text" name = "proprietaire" placeholder ={voiture.proprietaire} required/>
-        <button type = "submit">Ajouter</button>
+    <form onSubmit={handleSubmit}>
+        <input type = "text" name = "modele" value = {formData.modele} onChange={handleChange} required/>
+        <input type = "text" name = "prix" value = {formData.prix} onChange={handleChange} required/>
+        <input type = "text" name = "description" value = {formData.description} onChange={handleChange} required/>
+        <input type = "text" name = "image" value = {formData.image} onChange={handleChange} required/>
+        <input type = "text" name = "date" value = {formData.date} onChange={handleChange} required/>
+        <input type = "text" name = "proprietaire" value = {formData.proprietaire} onChange={handleChange} required/>
+        <button type = "submit">Mettre à jour le véhicule</button>
     </form>
 </div>
 );
